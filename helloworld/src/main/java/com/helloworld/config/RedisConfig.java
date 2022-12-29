6fa700ca9fc85908c8e4dd546c88b5c88610a5d9
정용원 <yongdev>
2022-12-29 16:32:46 +0900

    redis pubsub test (sorted set)

--- helloworld/src/main/java/com/helloworld/config/RedisConfig.java
+++ helloworld/src/main/java/com/helloworld/config/RedisConfig.java
@@ -2,10 +2,13 @@ package com.helloworld.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.cache.CacheKeyPrefix;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -13,7 +16,7 @@ import java.time.Duration;
 import java.util.HashMap;
 
 @Configuration
-public class RedisCacheConfig {
+public class RedisConfig {
 
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
@@ -37,5 +40,18 @@ public class RedisCacheConfig {
                 .cacheDefaults(configuration)
                 .withInitialCacheConfigurations(configMap)
                 .build();
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean(name = "redisListenContainer_primary")
+    @Primary
+    RedisMessageListenerContainer redisContainer() { //pub sub 이용할 수 있게 하는 container
+        final RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(redisConnectionFactory());
+        return container;
     }
 }
