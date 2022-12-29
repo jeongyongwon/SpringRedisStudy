bb9adab3c4ab33c1173d881eda361a99d2119a93
정용원 <yongdev>
2022-12-29 16:09:26 +0900

    redis ranking logic test (sorted set)

--- helloworld/src/main/java/com/HelloworldApplication.java
+++ helloworld/src/main/java/com/HelloworldApplication.java
@@ -1,15 +1,28 @@
 package com;
 
+import com.helloworld.service.ChatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-public class HelloworldApplication {
+public class HelloworldApplication implements CommandLineRunner {
+
+    @Autowired
+    private ChatService chatService;
 
     public static void main(String[] args) {
         SpringApplication.run(HelloworldApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        System.out.println("Application started..");
+
+        chatService.enterCharRoom("chat1");
     }
 
 }
